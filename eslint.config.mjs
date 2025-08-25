cc0d8da0f3bc05ad497a8441a67cd099ed0b5b4a
Aditya <jaiswal.aditya.deelip@gmail.com>
2025-08-25 22:17:27 +0530

    setup

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import pluginJs from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
